592c21981eb8165c4ea2b0db204b9c5b0659793a
amshelhack3r <33036027+amshelhack3r@users.noreply.github.com>
2020-10-19 12:54:03 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./styles.css";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "",
+  cache: new InMemoryCache()
+});
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ApolloProvider client={client}>
+        <h2>My first apollo app</h2>
+      </ApolloProvider>
     </div>
   );
 }
